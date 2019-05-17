07982e912d0a466b9345805ecafdc6d2dc93b734
Benjamin Schou Knudsen <benjaschou12@yahoo.dk>
2019-05-17 11:31:43 +0200

    Næsten det færdige resultat af navbar

--- js.js
+++ js.js
@@ -2,10 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "5px 10px";
-    document.getElementById("logo").style.fontSize = "5px";
+    document.getElementById("navbar").style.padding = "2px 10px";
+    document.getElementById("logo").style.width = "4%";
   } else {
-    document.getElementById("navbar").style.padding = "15px 10px";
-    document.getElementById("logo").style.fontSize = "15px";
+    document.getElementById("navbar").style.padding = "10px 10px";
+    document.getElementById("logo").style.width = "8%";
   }
 }
